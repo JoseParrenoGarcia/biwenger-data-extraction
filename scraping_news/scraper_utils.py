e306c25501e581ca6d5630b10e597d1d4ff697a5
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2025-08-28 16:49:24 +0200

    Trying to fix jornada perfecta scraping

--- scraping_news/scraper_utils.py
+++ scraping_news/scraper_utils.py
@@ -9,10 +9,15 @@ from datetime import datetime
 # Headers to mimic a real browser and avoid being blocked by websites
 HEADERS = {
     "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/117.0.0.0 Safari/537.36"
-    )
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive",
+    "DNT": "1",  # Do Not Track header
+    "Upgrade-Insecure-Requests": "1"
 }
 
 class Website:
